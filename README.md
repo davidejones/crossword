d379a34af5ad87f9f19363b32e08a1c8b76a72ba
davidejones <david@davidejones.com>
2024-03-28 12:47:37 +0000

    cleanup

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 A simple crossword puzzle using tkinter
 
-Thank you to linguapress for permission to use their crossword and clues which can be found here
+Thank you to linguapress for permission to use their crossword puzzle and clues which can be found here
 https://linguapress.com/puzzles/weather-crossword.htm
--- main.py
+++ main.py
@@ -1,96 +1,144 @@
-from tkinter import Tk, Canvas, Label, Frame, simpledialog
+from tkinter import Tk, Canvas, Label, Frame, simpledialog, LEFT, RIGHT, BOTH, Y, X
 
+class Puzzle:
 
-class PuzzleCanvas(Canvas):
-
-    def __init__(self, master=None, answers=None, cnf={}, **kw):
-        self.width = 605
-        self.height = 605
-        kwargs = {
-            "width": self.width,
-            "height": self.height,
-            "borderwidth": 0,
-            "highlightthickness": 0,
-            "relief": "solid",
-            "bg": "#000000"
-        }
-        super().__init__(master, cnf, **kwargs)
-        self.place(width=self.width, height=self.height, x=1024 - self.width - 50, y=50)
-        self.config(highlightbackground="grey", highlightcolor="grey")
+    def __init__(self, master=None, answers=None, across_clues=None, down_clues=None):
         self.tiles = []
 
-        self.delete('all')
-        w, h = 50, 50
-        px, py = 2, 2
+        # setup frames for left and right
+        self.clue_frame = Frame(master, bd=0, relief='solid')
+        self.clue_frame.pack(side=LEFT, fill=Y, padx=20, pady=50)
+        self.puzzle_frame = Frame(master, bd=0, relief='solid')
+        self.puzzle_frame.pack(side=RIGHT, fill=BOTH, expand=True, padx=0, pady=50)
+
+        # add clue label titles for down and across
+        Label(self.clue_frame, text="Across", font='Arial 16 bold').grid(row=0, column=0, sticky="nw")
+        Label(self.clue_frame, text="Down", font='Arial 16 bold').grid(row=0, column=1, sticky="nw")
+
+        # setup canvas for drawing the puzzle on right side
+        self.puzzle_canvas = Canvas(self.puzzle_frame, bg="#000000", relief="solid", borderwidth=0, highlightthickness=0)
+        self.puzzle_canvas.place(width=605, height=605)
+
+        # setup square dimensions and padding
+        square_w, square_h = 50, 50
+        square_px, square_py = 2, 2
+
         num_count = 1
+        across_clue_index, down_clue_index = 0, 0
+
+        self.puzzle_canvas.bind_all('<Key>', self.key_press)
+
         for y, row in enumerate(answers):
-            prev_x_char = ''
-            for x, char in enumerate(row):
-                next_x_char = row[x + 1] if x < len(row) - 1 else ''
-                next_x2_char = row[x + 2] if x < len(row) - 2 else ''
-                prev_y_char = answers[y - 1][x] if y > 0 else ''
-                next_y_char = answers[y + 1][x] if y < len(answers) - 1 else ''
-                x1 = (x * w) + px
-                y1 = (y * h) + py
-                if char == '_':
-                    self.tiles.append(
-                        self.create_rectangle(x1, y1, x1 + w, y1 + h, fill='black', outline='black', tags='')
-                    )
+            for x, current_char in enumerate(row):
+                # lets assign some understandable variables for checking
+                start_word = False
+                prev_char = row[x - 1] if x > 0 else ''
+                next_char = row[x + 1] if x < len(row) - 1 else ''
+                next_char2 = row[x + 2] if x < len(row) - 2 else ''
+                prev_char_above = answers[y - 1][x] if y > 0 else ''
+                next_char_below = answers[y + 1][x] if y < len(answers) - 1 else ''
+                is_first_char = x == 0
+                is_next_2_chars_valid = next_char != '_' and next_char2 != '_'
+                is_first_row = y == 0
+
+                # setup rect sizes
+                x1 = (x * square_w) + square_px
+                y1 = (y * square_h) + square_py
+
+                if current_char != '_':
+                    rect_kwargs = {'fill': 'white', 'tags': 'word'}
+                    #  (Across)
+                    #  if we are on the leftmost character, and it's a word (3 or more) OR
+                    #  we are starting a new word on the row, and it's not the end
+                    if (is_first_char and is_next_2_chars_valid) or (prev_char == '_' and next_char not in ('_', '')):
+                        self.puzzle_canvas.create_text(x1 + 1 + 10, y1 + 1 + 10, text=f"{num_count}.", fill="black", font='Arial 8 bold')
+                        Label(self.clue_frame, text=f'{num_count}. {across_clues[across_clue_index]}', font='Arial 10',
+                              wraplength=170, justify='left').grid(row=across_clue_index + 1, column=0, sticky="nw",
+                                                                   ipady=5)
+                        across_clue_index = across_clue_index + 1
+                        start_word = True
+
+                    # (Down)
+                    # if we are the topmost character, and it's a word OR
+                    # We are starting a new word on the column, and it's not the end
+                    if (is_first_row and next_char_below != '_') or (prev_char_above == '_' and next_char_below not in ('_', '')):
+                        self.puzzle_canvas.create_text(x1 + 1 + 10, y1 + 1 + 10, text=f"{num_count}.", fill="black", font='Arial 8 bold')
+                        Label(self.clue_frame, text=f'{num_count}. {down_clues[down_clue_index]}', font='Arial 10',
+                              wraplength=170, justify='left').grid(row=down_clue_index + 1, column=1, sticky="nw",
+                                                                   ipady=5)
+                        down_clue_index = down_clue_index + 1
+                        start_word = True
+
+                    if start_word:
+                        num_count += 1
                 else:
-                    self.tiles.append(
-                        self.create_rectangle(x1, y1, x1 + w, y1 + h, fill='white', outline='black', tags='active')
-                    )
-                    # is start of column or previous char is inactive then write num
-                    if (x == 0 and next_x_char != '_' and next_x2_char != '_') or (prev_x_char == '_' and x < len(row)-1 and next_x_char != '_'):
-                        # if this is a new across word then start number label
-                        Label(self, text=f"{num_count}.", font='Arial 8 bold', bg='white').place(x=x1+1, y=y1+1)
-                        num_count = num_count + 1
-                    elif (y == 0 and next_y_char != '_') or (prev_y_char == '_' and y < len(answers)-1 and next_y_char != '_'):
-                        # if this is a new down word then start number label
-                        Label(self, text=f"{num_count}.", font='Arial 8 bold', bg='white').place(x=x1 + 1, y=y1 + 1)
-                        num_count = num_count + 1
-                self.tag_bind(self.tiles[-1], '<Enter>', lambda event, obj=self.tiles[-1]: self.mouse_enter(event, obj))
-                self.tag_bind(self.tiles[-1], '<Leave>', lambda event, obj=self.tiles[-1]: self.mouse_exit(event, obj))
-                self.tag_bind(self.tiles[-1], '<Button-1>', lambda event, obj=self.tiles[-1]: self.mouse_click(event, obj))
-                prev_x_char = char
+                    rect_kwargs = {'fill': 'black', 'tags': ''}
+
+                # let's create each rectangle black/white and assign event handlers and lower below numbers
+                rect = self.puzzle_canvas.create_rectangle(x1, y1, x1 + square_w, y1 + square_h, **rect_kwargs)
+                self.puzzle_canvas.tag_lower(rect)
+                if 'word' in rect_kwargs['tags']:
+                    self.puzzle_canvas.tag_bind(rect, '<Enter>', lambda event, obj=rect: self.mouse_enter(event, obj))
+                    self.puzzle_canvas.tag_bind(rect, '<Leave>', lambda event, obj=rect: self.mouse_exit(event, obj))
+                    self.puzzle_canvas.tag_bind(rect, '<Button-1>', lambda event, obj=rect: self.mouse_click(event, obj))
 
     def mouse_enter(self, event, obj):
-        if 'active' in self.gettags("current"):
-            self.itemconfig(obj, fill="grey")
+        """
+        Set square grey on mouse enter
+        :param event: event object
+        :param obj: object on canvas
+        """
+        if 'active' not in self.puzzle_canvas.gettags("current"):
+            self.puzzle_canvas.itemconfig(obj, fill="grey")
 
     def mouse_exit(self, event, obj):
-        if 'active' in self.gettags("current"):
-            self.itemconfig(obj, fill="white")
+        """
+        Set square white on mouse exit
+        :param event: event object
+        :param obj: object on canvas
+        """
+        if 'active' not in self.puzzle_canvas.gettags("current"):
+            self.puzzle_canvas.itemconfig(obj, fill="white")
 
     def mouse_click(self, event, obj):
-        if 'active' in self.gettags("current"):
-            #print("Click by caller", event, obj)
-            input = simpledialog.askstring(title="Answer", prompt="What's your Answer?:")
-            print(input)
+        """
+        Take user input on mouse clicking a square
+        :param event: event object
+        :param obj: object on canvas
+        """
+        self.clear_active()
+        self.set_active(obj)
 
-class CluesCanvas(Canvas):
+    def key_press(self, event):
+        # find active one and set the Character to event.char.
+        for item in self.puzzle_canvas.find_withtag('active'):
+            coords = self.puzzle_canvas.coords(item)
+            self.puzzle_canvas.create_text(coords[0] + 25, coords[1] + 25, text=event.char.upper(), fill="black", font='Arial 14 bold')
+        # increment active pointer
+        self.clear_active()
 
-    def __init__(self, master=None, across_clues=None, down_clues=None, cnf={}, **kwargs):
-        super().__init__(master, cnf, **kwargs)
-        self.across_clues = across_clues
-        self.down_clues = down_clues
-        self.frame = Frame()
-        self.frame.place(x=20, y=50)
-        Label(self.frame, text="Across", font='Arial 16 bold').grid(row=0, column=0, sticky="nw")
-        Label(self.frame, text="Down", font='Arial 16 bold').grid(row=0, column=1, sticky="nw")
-        num = 0
-        for row, clue in enumerate(zip(self.across_clues, self.down_clues), start=1):
-            across_clue, down_clue = clue
-            num += 1
-            Label(self.frame, text=f'{num}.{across_clue}', font='Arial 10', wraplength=170, justify='left').grid(row=row, column=0, sticky="nw", ipady=5)
-            num += 1
-            Label(self.frame, text=f'{num}.{down_clue}', font='Arial 10', wraplength=170, justify='left').grid(row=row, column=1, sticky="nw", ipady=5)
+    def clear_active(self):
+        # clear existing active tags
+        for item in self.puzzle_canvas.find_withtag('active'):
+            item_tags = self.puzzle_canvas.gettags(item)
+            clean_tags = list(filter(lambda x: (x != 'active'), item_tags))
+            self.puzzle_canvas.itemconfig(item, tags=clean_tags)
+            self.puzzle_canvas.itemconfig(item, fill="white")
 
+    def set_active(self, obj):
+        # add active tag and set color
+        tags = self.puzzle_canvas.gettags("current")
+        tags += ('active',)
+        self.puzzle_canvas.itemconfig(obj, tags=tags)
+        self.puzzle_canvas.itemconfig(obj, fill="#ADD8E6")
 
 class CrosswordApp(Tk):
 
     def __init__(self, screenName=None, baseName=None, className='Tk', useTk=1, sync=0, use=None):
-
+        """
+        Thank you to linguapress for permission to use their crossword puzzle and clues which can be found here
+        https://linguapress.com/puzzles/weather-crossword.htm
+        """
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.minsize(1024, 718)
         self.maxsize(1024, 718)
@@ -135,8 +183,7 @@ class CrosswordApp(Tk):
             "People wear them when no.11 is too strong",
             "You cannot see very far through this",
         ]
-        self.puzzle = PuzzleCanvas(self, self.answers)
-        self.clues = CluesCanvas(self, self.across_clues, self.down_clues)
+        self.puzzle = Puzzle(self, self.answers, self.across_clues, self.down_clues)
         self.title("Crossword")
         self.draw()
 
